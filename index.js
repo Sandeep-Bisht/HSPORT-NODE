02ce69127ac4a622f95a6ba9f25089d8c6b32e67
Sandeep Bisht <>
2023-06-27 10:55:04 +0530

    product api

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const db =require('./config/database')
 const app = express();
+const bodyParser=require('body-parser');
 require('dotenv').config();
+
+const LoginRouter=require('./Api/Authantication/AuthRouting');
+const ProductRouter = require('./Api/Product/ProductRouting');
 
 
 db();
 app.use(express.json());
 
-console.log("hindustan sportd");
+const cors = require('cors');
+require('dotenv').config()
+app.use(bodyParser.json({ limit: "100mb", extended: true, parameterLimit: 50000 }));
+app.use(bodyParser.urlencoded({ limit: "100mb", extended: true, parameterLimit: 50000 }))
+app.use(cors({ origin: true }));
+app.use('/public', express.static('public'));
+app.use('/api/auth',LoginRouter);
+app.use('/api/product',ProductRouter);
 
 const port = process.env.PORT || 8080;
 
