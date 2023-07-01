0793f0a449a3970736f7d16c6d1f0578e08927d6
Deepak Negi <deepak.negi.giksindia.com>
2023-07-01 15:07:05 +0530

    add slug on product api

--- Api/Product/ProductController.js
+++ Api/Product/ProductController.js
@@ -1,41 +1,48 @@
 const ProductService = require("./ProductService");
+const slugify = require("slugify");
+
 module.exports = {
   create: (req, res) => {
-        try {
-          var data = {...req.body,image:req.files.image, otherImage:req.files.otherImage};      
-          ProductService.create(data).then((result) => {
-            if (result) {
-               res.status(200).json({
-                success: 200,
-                message: "Product Created succefully",
-              });
-            } else {
-               res.json({
-                success: 400,
-                message: "Please provide correct information",
-              });
-            }
-          }).catch((err) => {
-            if (err.code === 11000) {
-              res.status(400).json({
-                success: 400,
-                message: "Product already exists",
-              });
-            } else {
-              console.log(err);
-              res.json({
-                success: 400,
-                message: "Please provide correct information",
-              });
-            }
-          });
-        } catch (err) {
-          console.log(err);
-          res.json({
-            success: 400,
-            message: "Please provide correct information",
-          });
-        }
+    try {
+      const { name, ...otherData } = req.body;
+      const slug = slugify(name, { lower: true });
+      const data = { ...otherData, name, slug, image: req.files.image, otherImage: req.files.otherImage };
+
+      ProductService.create(data)
+        .then((result) => {
+          if (result) {
+            res.status(200).json({
+              success: 200,
+              message: "Product Created successfully",
+            });
+          } else {
+            res.json({
+              success: 400,
+              message: "Please provide correct information",
+            });
+          }
+        })
+        .catch((err) => {
+          if (err.code === 11000) {
+            res.status(400).json({
+              success: 400,
+              message: "Product already exists",
+            });
+          } else {
+            console.log(err);
+            res.json({
+              success: 400,
+              message: "Please provide correct information",
+            });
+          }
+        });
+    } catch (err) {
+      console.log(err);
+      res.json({
+        success: 400,
+        message: "Please provide correct information",
+      });
+    }
   },
   find_all: (req, res, next) => {    
       try {            
@@ -88,53 +95,57 @@ module.exports = {
     }
   },
   find_and_update:(req,res,next)=>{
-    const {_id,name,description,warehouse,category,subcategory,quantity,reorderQuantity,maximumOrder,inrMrp,dollerMrp,inrDiscount,dollerDiscount,manufacturer,type} =req.body;
-    const data={
-      _id:_id,
-      name:name,
-      description:description,
-      warehouse:warehouse,
-      category:category,
-      subcategory:subcategory,
-      quantity:quantity,
-      reorderQuantity:reorderQuantity,
-      maximumOrder:maximumOrder,
-      inrMrp:inrMrp,
-      dollerMrp:dollerMrp,
-      inrDiscount:inrDiscount,
-      dollerDiscount:dollerDiscount,
-      manufacturer:manufacturer,
-      type:type,
-    }   
-    if(req.files.image){
-      data.image = req.files.image;     
-    } 
-    if(req.files.image){
-      data.otherImage = req.files.otherImage;     
+    const { _id, name, description, warehouse, category, subcategory, quantity, reorderQuantity, maximumOrder, inrMrp, dollerMrp, inrDiscount, dollerDiscount, manufacturer, type } = req.body;
+    const slug = slugify(name, { lower: true });
+    
+    const data = {
+      _id: _id,
+      name: name,
+      slug: slug,
+      description: description,
+      warehouse: warehouse,
+      category: category,
+      subcategory: subcategory,
+      quantity: quantity,
+      reorderQuantity: reorderQuantity,
+      maximumOrder: maximumOrder,
+      inrMrp: inrMrp,
+      dollerMrp: dollerMrp,
+      inrDiscount: inrDiscount,
+      dollerDiscount: dollerDiscount,
+      manufacturer: manufacturer,
+      type: type,
+    };
+    
+    if (req.files.image) {
+      data.image = req.files.image;
     }
-    try{  
-      ProductService.find_and_update(_id,data).then((result) => {      
-        if (result) {  
+    if (req.files.image) {
+      data.otherImage = req.files.otherImage;
+    }
+    
+    try {
+      ProductService.find_and_update(_id, data).then((result) => {
+        if (result) {
           res.status(200).json({
             data: result,
-            msg:'data found'
+            msg: 'data found'
           });
-               
         } else {
           res.json({
             error: 400,
             message: "Data Not Found",
           });
         }
-      })
+      });
+    } catch (err) {
+      console.log(err);
+      res.json({
+        success: 400,
+        message: "Please provide correct information",
+      });
     }
-     catch (err) {
-        console.log(err);
-        res.json({
-          success: 400,
-          message: "Please provide correct information",
-        });
-      }
+    
     },
   find_and_delete:(req,res)=>{
     const {_id} = req.body
